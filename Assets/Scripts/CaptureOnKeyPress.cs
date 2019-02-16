efb72b0edf51d374aa86fea283e326928a1eb336
dan-r95 <d.rossburg@googlemail.com>
2019-02-16 21:40:33 +0100

    3 different capture approaches, see scene explorer

---
+++ Assets/Scripts/CaptureOnKeyPress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RockVR.Video;
+using RockVR.Common;
+
+
+public class CaptureOnKeyPress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.C)) {
+			VideoCaptureCtrl.instance.StartCapture();
+		}
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			VideoCaptureCtrl.instance.StopCapture();
+		}
+    }
+}
+
+
